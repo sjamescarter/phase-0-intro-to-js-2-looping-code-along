614880973ce23a70a01c9191d3f0f1ef4b3122f7
sjamescarter <stephenjamescarter@gmail.com>
2022-11-28 10:27:36 -0600

    commit tree 70c47494d6ba0672b18b2fa3a61cf34cac205520

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+    let message = [];
+    for (let i = 0; i < name.length; i++) {
+        message[i] = `Thank you, ${name[i]}, for the wonderful ${event} gift!`
+    };
+
+    return message;
+}
+
+function countDown(i) {
+    while (i >= 0) {
+        console.log(i--)
+    };
+}
